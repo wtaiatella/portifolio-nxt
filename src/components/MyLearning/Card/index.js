a3bbf2c8dacb8129b823ca6d4d6b10c334955a68
Wagner Taiatella <wtaiatella@gmail>
2022-09-08 17:14:03 -0400

    chore: add card for learning section

--- src/components/MyLearning/Card/index.js
+++ src/components/MyLearning/Card/index.js
@@ -1,24 +1,67 @@
+import Modal from 'react-modal';
 import { Container } from './styles';
+import { course } from '../courses.json';
+import { useState } from 'react';
+import ReactMarkdown from 'markdown-to-jsx';
+import file from '../README.md';
+
+const customStyles = {
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		maxWidth: '1300px',
+		maxHeight: '700px',
+		zIndex: '9999',
+
+		transform: 'translate(-50%, -50%)',
+	},
+};
+// Modal.setAppElement(document.getElementById('root'));
 
 export function Card() {
+	const { name, description } = course[0];
+	console.log(course[0].name);
+
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
+
 	return (
-		<Container>
-			<figure className='snip1477'>
-				<img src='/img/logos/sample38.jpg' alt='sample38' />
-				<div className='title'>
-					<div>
-						<h2>Penny</h2>
-						<h4>Tool</h4>
+		<>
+			<Container>
+				<figure className='snip1477'>
+					<img src='/img/logos/origamid logo.png' alt='sample38' />
+					<div className='title'>
+						<div>
+							<h2>{name}</h2>
+						</div>
 					</div>
-				</div>
-				<figcaption>
-					<p>
-						Which is worse, that everyone has his price, or that the
-						price is always so low.
-					</p>
-				</figcaption>
-				<a href='#'></a>
-			</figure>
-		</Container>
+					<figcaption>
+						<p>{description}</p>
+					</figcaption>
+					<a onClick={openModal}></a>
+				</figure>
+				<Modal
+					isOpen={isModalOpen}
+					onRequestClose={closeModal}
+					style={customStyles}
+					contentLabel='Example Modal'
+				>
+					<ReactMarkdown
+						// eslint-disable-next-line react/no-children-prop
+						children={file}
+					/>
+				</Modal>
+			</Container>
+		</>
 	);
 }
--- src/components/MyLearning/Card/styles.js
+++ src/components/MyLearning/Card/styles.js
@@ -1,146 +1,153 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-	@import url(https://fonts.googleapis.com/css?family=Raleway:400,500,700);
-	@import url(https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css);
 	figure.snip1477 {
-		font-family: 'Raleway', Arial, sans-serif;
+		font-family: 'Titillium Web', sans-serif;
+
 		position: relative;
 		overflow: hidden;
-		margin: 10px;
-		min-width: 230px;
-		max-width: 315px;
+		margin: 0px;
+		min-width: 200px;
+		max-width: 300px;
 		width: 100%;
-		color: #ffffff;
-		text-align: center;
-		font-size: 16px;
-		background-color: #000000;
-	}
-	figure.snip1477 *,
-	figure.snip1477 *:before,
-	figure.snip1477 *:after {
-		-webkit-box-sizing: border-box;
-		box-sizing: border-box;
-		-webkit-transition: all 0.55s ease;
-		transition: all 0.55s ease;
-	}
-	figure.snip1477 img {
-		max-width: 100%;
-		backface-visibility: hidden;
-		vertical-align: top;
-		opacity: 0.9;
-	}
-	figure.snip1477 .title {
-		position: absolute;
-		top: 58%;
-		left: 25px;
-		padding: 5px 10px 10px;
-	}
-	figure.snip1477 .title:before,
-	figure.snip1477 .title:after {
-		height: 2px;
-		width: 400px;
-		position: absolute;
-		content: '';
-		background-color: #ffffff;
-	}
-	figure.snip1477 .title:before {
-		top: 0;
-		left: 10px;
-		-webkit-transform: translateX(100%);
-		transform: translateX(100%);
-	}
-	figure.snip1477 .title:after {
-		bottom: 0;
-		right: 10px;
-		-webkit-transform: translateX(-100%);
-		transform: translateX(-100%);
-	}
-	figure.snip1477 .title div:before,
-	figure.snip1477 .title div:after {
-		width: 2px;
-		height: 300px;
-		position: absolute;
-		content: '';
-		background-color: #ffffff;
-	}
-	figure.snip1477 .title div:before {
-		top: 10px;
-		right: 0;
-		-webkit-transform: translateY(100%);
-		transform: translateY(100%);
-	}
-	figure.snip1477 .title div:after {
-		bottom: 10px;
-		left: 0;
-		-webkit-transform: translateY(-100%);
-		transform: translateY(-100%);
-	}
-	figure.snip1477 h2,
-	figure.snip1477 h4 {
-		margin: 0;
-		text-transform: uppercase;
-	}
-	figure.snip1477 h2 {
-		font-weight: 400;
-	}
-	figure.snip1477 h4 {
-		display: block;
-		font-weight: 700;
-		background-color: #ffffff;
-		padding: 5px 10px;
-		color: #000000;
-	}
-	figure.snip1477 figcaption {
-		position: absolute;
-		bottom: 42%;
-		left: 25px;
+		color: white;
 		text-align: left;
-		opacity: 0;
-		padding: 5px 60px 5px 10px;
-		font-size: 0.8em;
-		font-weight: 500;
-		letter-spacing: 1.5px;
+		font-size: 1.1em;
+		background-color: var(--color-back-dark);
+
+		*,
+		*:before,
+		*:after {
+			-webkit-box-sizing: border-box;
+			box-sizing: border-box;
+			-webkit-transition: all 0.55s ease;
+			transition: all 0.55s ease;
+		}
+		img {
+			max-width: 100%;
+			backface-visibility: hidden;
+			vertical-align: top;
+			opacity: 0.9;
+		}
+		.title {
+			position: absolute;
+			top: 60%;
+			left: 25px;
+			padding: 5px 8px 5px;
+			max-width: 80%;
+
+			h2 {
+				font-weight: bold;
+				color: var(--color-text-dark);
+				margin: 0;
+				text-transform: uppercase;
+				line-height: 35px;
+			}
+
+			:before,
+			:after {
+				height: 2px;
+				width: 400px;
+				position: absolute;
+				content: '';
+				background-color: #ffffff;
+			}
+
+			:before {
+				top: 0;
+				left: 10px;
+				-webkit-transform: translateX(100%);
+				transform: translateX(100%);
+			}
+			:after {
+				bottom: 0;
+				right: 10px;
+				-webkit-transform: translateX(-100%);
+				transform: translateX(-100%);
+			}
+
+			div:before,
+			div:after {
+				width: 2px;
+				height: 300px;
+				position: absolute;
+				content: '';
+				background-color: #ffffff;
+			}
+			div:before {
+				top: 10px;
+				right: 0;
+				-webkit-transform: translateY(100%);
+				transform: translateY(100%);
+			}
+
+			div:after {
+				bottom: 10px;
+				left: 0;
+				-webkit-transform: translateY(-100%);
+				transform: translateY(-100%);
+			}
+		}
+
+		figcaption {
+			position: absolute;
+			top: 50%;
+			left: 25px;
+			text-align: left;
+			opacity: 0;
+			padding: 5px 60px 5px 10px;
+			font-size: 0.8em;
+			font-weight: 500;
+			letter-spacing: 1.5px;
+
+			p {
+				margin: 0;
+			}
+		}
+
+		a {
+			position: absolute;
+			top: 0;
+			bottom: 0;
+			left: 0;
+			right: 0;
+		}
 	}
-	figure.snip1477 figcaption p {
-		margin: 0;
-	}
-	figure.snip1477 a {
-		position: absolute;
-		top: 0;
-		bottom: 0;
-		left: 0;
-		right: 0;
-	}
-	figure.snip1477:hover img,
-	figure.snip1477.hover img {
-		zoom: 1;
-		filter: alpha(opacity=35);
-		-webkit-opacity: 0.35;
-		opacity: 0.35;
-	}
-	figure.snip1477:hover .title:before,
-	figure.snip1477.hover .title:before,
-	figure.snip1477:hover .title:after,
-	figure.snip1477.hover .title:after,
-	figure.snip1477:hover .title div:before,
-	figure.snip1477.hover .title div:before,
-	figure.snip1477:hover .title div:after,
-	figure.snip1477.hover .title div:after {
-		-webkit-transform: translate(0, 0);
-		transform: translate(0, 0);
-	}
-	figure.snip1477:hover .title:before,
-	figure.snip1477.hover .title:before,
-	figure.snip1477:hover .title:after,
-	figure.snip1477.hover .title:after {
-		-webkit-transition-delay: 0.15s;
-		transition-delay: 0.15s;
-	}
-	figure.snip1477:hover figcaption,
-	figure.snip1477.hover figcaption {
-		opacity: 1;
-		-webkit-transition-delay: 0.2s;
-		transition-delay: 0.2s;
+
+	figure.snip1477:hover {
+		img {
+			zoom: 1;
+			filter: alpha(opacity=35);
+			-webkit-opacity: 0.35;
+			opacity: 0.35;
+		}
+
+		.title {
+			:before,
+			:after,
+			div:before,
+			div:after {
+				-webkit-transform: translate(0, 0);
+				transform: translate(0, 0);
+			}
+
+			:before,
+			:after {
+				-webkit-transition-delay: 0.15s;
+				transition-delay: 0.15s;
+			}
+
+			h2 {
+				color: var(--color-text-orange);
+				-webkit-transition-delay: 0.15s;
+				transition-delay: 0.15s;
+			}
+		}
+
+		figcaption {
+			opacity: 1;
+			-webkit-transition-delay: 0.2s;
+			transition-delay: 0.2s;
+		}
 	}
 `;
