582c5a0cb25b908f1f1dea36951086fb2282f7de
Wagner Taiatella <wtaiatella@gmail>
2022-09-08 17:13:24 -0400

    feat: add lib to read MD files

--- src/components/MyLearning/Grid/index.js
+++ src/components/MyLearning/Grid/index.js
@@ -1,5 +1,17 @@
+import { useEffect, useState } from 'react';
+import ReactMarkdown from 'markdown-to-jsx';
+import file from '../README.md';
+
 import { Container } from './styles';
 
 export function Grid() {
-	return <Container></Container>;
+	return (
+		<Container>
+			<ReactMarkdown
+				// eslint-disable-next-line react/no-children-prop
+				children={file}
+			/>
+			{/* <ReactMarkdown>`${file}`</ReactMarkdown>{' '} */}
+		</Container>
+	);
 }
