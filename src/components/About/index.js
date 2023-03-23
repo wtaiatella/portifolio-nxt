7c0ba204d5696cdaef943b840e5ba5413a6e1268
Wagner Taiatella <wtaiatella@gmail>
2023-03-23 15:11:30 -0300

    chore: update aboutme description

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,32 +10,35 @@ export function About() {
 				<Title>About me</Title>
 
 				<p>
-					Enthusiastic about technologies, I spent good part of my
-					professional career programming equipment for the automation
-					of power plant substations, but I always wanted something
-					more. After I had contact with web development because of a
-					personal project, I realized that this is what I wanted to
-					do from now on.
+					During my professional career I worked as a substation
+					automation programmer building robust solutions that would
+					guarantee the safety of the electrical power system
+					operation. At that time I developed control logics,
+					performed tests on them and configured ethernet network
+					architectures.
 				</p>
 
 				<p>
-					Knowing this, as a self-taught person with a lot of
+					But some years ago I had the opportunity to learn a little
+					about web development due a personal project and I loved it
+					very much. After this, as a self-taught person with a lot of
 					resilience, I became interested in the world of web
 					development and in my spare time, with great help from
-					Google and Developer Communities, I learned Javascript,
-					React and Nextjs.
+					Google and Developer Communities, I learned some important
+					concepts about HTML, CSS, Javascript, Node, React, Nextjs,
+					responsive web pages, APIs, Git an GitHub and React
+					Components
 				</p>
 
 				<p>
-					It&apos;s been two years since I started studying
-					programming always learning new techniques to become a
+					Since then, I always search for new techniques to become a
 					better programmer and to help me combine these technologies.
 				</p>
 
 				<p>
-					Throughout my studies I developed several test pages and
-					gathered here in this portfolio the ones I consider most
-					important. Hope you like!!
+					Throughout my studies I developed several pages and gathered
+					here in this portfolio the ones I consider most important.
+					Hope you like!!
 				</p>
 			</div>
 		</Container>
