5869e2c7042d192c77c3902a8c28caa63bd1985d
Wagner Taiatella <wtaiatella@gmail.com>
2024-04-11 20:43:35 -0300

    codeDeploy: add folder change in AfterInstall step

--- scripts/deployAfterInstall.sh
+++ scripts/deployAfterInstall.sh
@@ -3,5 +3,6 @@ cd /var/www/
 sudo chown -R ec2-user:ec2-user wtaiatella
 cd wtaiatella
 source ~/.nvm/nvm.sh
+cd /var/www/wtaiatella
 ~/.nvm/versions/node/v20.12.1/bin/npm install
 ~/.nvm/versions/node/v20.12.1/bin/npm run build
