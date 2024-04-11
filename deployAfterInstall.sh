939163e2779ea4a7670f7cf0159e9cf70fd8cb9d
Wagner Taiatella <wtaiatella@gmail.com>
2024-04-11 14:33:24 -0300

    codeDeploy: update project path

--- deployAfterInstall.sh
+++ deployAfterInstall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd /var/www/wtaiatella/
 source ~/.nvm/nvm.sh
 ~/.nvm/versions/node/v20.12.1/bin/npm run build
--- deployApplicationStart.sh
+++ deployApplicationStart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /var/www/wtaiatella/
 source ~/.nvm/nvm.sh
 ~/.nvm/versions/node/v20.12.1/bin/npm run start
 ~/.nvm/versions/node/v20.12.1/bin/pm2 start wtaiatella
