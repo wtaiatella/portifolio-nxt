2d29fa94b553571ed6a356423422367841174b03
Wagner Taiatella <wtaiatella@gmail.com>
2024-04-11 15:00:07 -0300

    codeDeploy: test update Intro

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,27 +1,27 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { Container } from './styles';
+import Image from "next/image";
+import Link from "next/link";
+import { Container } from "./styles";
 
 export function Intro() {
-	return (
-		<Container>
-			<p className='intro-title'>Hey there!</p>
-			<h1 className='intro-title'>I&apos;m Wagner Taiatella</h1>
-			<h2 className='intro-highlight'>Front End Web Developer</h2>
-			<h3 className='intro-focus'>focus on learning</h3>
-			<a href='#portifolio' className='intro-btn'>
-				KNOW MY WORK
-			</a>
-			<Image
-				alt='Mountains'
-				src='/img/header-image.png'
-				fill
-				style={{
-					objectFit: 'cover',
-					objectPosition: 'right botton',
-				}}
-				quality={100}
-			/>
-		</Container>
-	);
+  return (
+    <Container>
+      <p className="intro-title">Hey there!</p>
+      <h1 className="intro-title">I&apos;m Wagner Elias Taiatella</h1>
+      <h2 className="intro-highlight">Front End Web Developer</h2>
+      <h3 className="intro-focus">focus on learning</h3>
+      <a href="#portifolio" className="intro-btn">
+        KNOW MY WORK
+      </a>
+      <Image
+        alt="Mountains"
+        src="/img/header-image.png"
+        fill
+        style={{
+          objectFit: "cover",
+          objectPosition: "right botton",
+        }}
+        quality={100}
+      />
+    </Container>
+  );
 }
