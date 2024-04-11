120b6c2d9d90e0acc1d5b6f5be78da4dd3056b32
Wagner Taiatella <wtaiatella@gmail.com>
2024-04-11 17:32:12 -0300

    codeDeploy: move scripts

--- scripts/deployAfterInstall.sh
+++ scripts/deployAfterInstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cd /var/www/wtaiatella/
+cd /var/www/
+sudo chown -R ec2-user:ec2-user wtaiatella
+cd wtaiatella
 source ~/.nvm/nvm.sh
 ~/.nvm/versions/node/v20.12.1/bin/npm install
 ~/.nvm/versions/node/v20.12.1/bin/npm run build
