94c8ae011d54c766797cb5b2ec3806dd72c6669f
Wagner Taiatella <wtaiatella@gmail.com>
2024-04-11 23:08:11 -0300

    codeDeploy: make scripts executable

--- scripts/deployAfterInstall.sh
+++ scripts/deployAfterInstall.sh
@@ -4,5 +4,6 @@ sudo chown -R ec2-user:ec2-user wtaiatella
 cd wtaiatella
 source ~/.nvm/nvm.sh
 cd /var/www/wtaiatella
+chmod +x ./scripts/*
 ~/.nvm/versions/node/v20.12.1/bin/npm install
 ~/.nvm/versions/node/v20.12.1/bin/npm run build
--- scripts/deployBeforeInstall.sh
+++ scripts/deployBeforeInstall.sh
@@ -3,4 +3,3 @@ source ~/.nvm/nvm.sh
 ~/.nvm/versions/node/v20.12.1/bin/pm2 stop wtaiatella
 cd /var/www/wtaiatella
 find . -mindepth 1 ! -name '.env' -exec rm -rf {} +
-mkdir wtaiatella
