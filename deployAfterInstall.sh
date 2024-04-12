55071f3cd384f2e72002afc856d905145c79a38b
Wagner Taiatella <wtaiatella@gmail.com>
2024-04-12 12:32:34 -0300

    codeDeploy: add permitions and log messages

--- deployAfterInstall.sh
+++ deployAfterInstall.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 cd /var/www/
+echo "Alterando a propriedade da pasta wtaiatella"
 sudo chown -R ec2-user:ec2-user wtaiatella
 cd wtaiatella
-source ~/.nvm/nvm.sh
+echo "Mudando para o diretório wtaiatella"
 cd /var/www/wtaiatella
-chmod +x ./scripts/*
+echo "Carregando o ambiente do NVM"
+source ~/.nvm/nvm.sh
+echo "Instalando as dependências do projeto"
 ~/.nvm/versions/node/v20.12.1/bin/npm install
+echo "Executando o script de build do projeto"
 ~/.nvm/versions/node/v20.12.1/bin/npm run build
--- deployBeforeInstall.sh
+++ deployBeforeInstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-chmod +x ./*.sh
 source ~/.nvm/nvm.sh
 ~/.nvm/versions/node/v20.12.1/bin/pm2 stop wtaiatella
 cd /var/www/wtaiatella
